b98403dfe78f7ba7bf2d095aef079c4576ac4f2c
khelalfa rabeh <khelalfarabeh1996@gmail.com>
2020-03-31 17:46:29 +0100

    Create The Store

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import redcer from './reducers'
+
+const store = createStore(redcer)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
