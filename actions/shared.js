bd87e56827c1340c6a5f3f8afdeaba504f555bec
khelalfa rabeh <khelalfarabeh1996@gmail.com>
2020-03-31 17:27:00 +0100

    style: add some comments

--- actions/shared.js
+++ actions/shared.js
@@ -3,6 +3,8 @@ import { receiveUsers } from './users';
 import { receiveQuestions } from './questions';
 import { setAuthedUser } from '../actions/authedUser'
 
+// Make the authed user constant at first
+// Before we create the login and choose a user
 const AUTHED_ID = 'rabah_khelalfa'
 
 export function handleInitialData() {
