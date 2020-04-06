adf054b29cba27ff4447d0fbca0e14f882e691af
khelalfa rabeh <khelalfarabeh1996@gmail.com>
2020-04-06 22:05:53 +0100

    Create PollResults component that handle a poll results

---
+++ component/PollResults.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import OptionResults from './OptionResults';
+
+const PollResutls = (props) => {
+	const { author , questionId} = props
+	return(
+		<div className="mai.container">
+	      <div className="poll-results">
+	      	 <div className="user-card main-border">
+				<h5 className="card-header main-color">{author.name} asks:</h5>
+
+				<div className="card-body">
+				  <div className="card-avatar">
+				  	<img src={author.avatarURL} 
+				  		 alt="user avatar" 	
+				  		 className="big-avatar"/>
+				  </div>
+
+				  <div className="card-info">
+				  	<h3 className="main-color">Results</h3>
+				  	 <OptionResults option={"optionOne"} questionId={questionId} />
+				  	 <OptionResults option={"optionTwo"} questionId={questionId} />	
+				  </div>	
+				  <div className="clearfix"></div>
+				</div>
+			  </div>
+	      </div>
+		</div>
+		)
+}
+
+
+PollResutls.propTypes = {
+	author: PropTypes.object.isRequired,
+	questionId: PropTypes.string.isRequired,
+}
+
+export default PollResutls
