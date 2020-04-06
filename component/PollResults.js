604780b4fe057d03e3aa263573101551b80d5329
khelalfa rabeh <khelalfarabeh1996@gmail.com>
2020-04-06 22:32:51 +0100

    Fix the import error

--- component/PollResults.js
+++ component/PollResults.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import OptionResults from './OptionResults';
+import OptionResults from '../container/OptionResults';
 
 const PollResutls = (props) => {
 	const { author , questionId} = props
