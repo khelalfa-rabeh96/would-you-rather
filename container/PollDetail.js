90db05cb61192a205793e8156d7c041dcd6be0b9
khelalfa rabeh <khelalfarabeh1996@gmail.com>
2020-04-10 23:07:44 +0100

    feat: Show 404 Error when provide in the path a question id doesn't exist

--- container/PollDetail.js
+++ container/PollDetail.js
@@ -5,39 +5,66 @@ import PollResults from '../component/PollResults'
 import PollQuestion from './PollQuestion'
 
 const PollDetail = (props) => { 
-	const {questionId, pollStatus, author} = props
 
+	// If the question id in the path
+	// Doesn't exist diplay error message
+	if(props.trueId === false){
+		return (
+			<div class="alert alert-primary" role="alert">
+			 404 Error Question Not Found!
+			</div>
+		)
+	// Else 
 	// Return the appropriate component 
 	// If the authed User has answered that poll
 	// Than return the resultes  of that poll
 	// Else return the form to submit his answer
-	return(
-		<div className="poll-detail">
-			{ pollStatus === 'answred' ?
-			  <PollResults questionId={questionId} author={author}/>
-			  :
-			  <PollQuestion questionId={questionId} author={author}/>
-			  
-			}
-		</div>
+	}else{
+
+		const {questionId, pollStatus, author} = props
+		return(
+			<div className="poll-detail">
+				{ pollStatus === 'answred' ?
+				  <PollResults questionId={questionId} author={author}/>
+				  :
+				  <PollQuestion questionId={questionId} author={author}/>
+				  
+				}
+			</div>
 		)
+	}
+
+
+	
 }
 
 function mapStateToProps({authedUser, users,questions}, props){
-	const { questionId } = props.match.params
-	const aUser = users[authedUser];
-	const question = questions[questionId];
-	const author = users[question.author];
+	const { questionId } = props.match.params;
+	const trueId = Object.keys(questions).includes(questionId);
 
-	// check if the authed user has answered that question 
-	// And return the appropriate response 
-	const pollStatus = Object.keys(aUser.answers).includes(questionId) ? 'answred' : 'unanswred';
-	
-	return {
-		pollStatus,
-		author,
-		questionId
+	// Check if exist a  question with 
+	//the question id that provided in the path
+	if(trueId){
+		const aUser = users[authedUser];
+		const question = questions[questionId];
+		const author = users[question.author];
+
+		// check if the authed user has answered that question 
+		// And return the appropriate response 
+		const pollStatus = Object.keys(aUser.answers).includes(questionId) ? 'answred' : 'unanswred';
+		
+		return {
+			pollStatus,
+			author,
+			questionId,
+			trueId
+		}
+	}else{
+		return{
+			trueId
+		}
 	}
+	
 
 }
 
@@ -45,5 +72,6 @@ PollDetail.propTypes = {
 	questionId: PropTypes.string.isRequired,
 	pollStatus: PropTypes.string.isRequired,
 	author: PropTypes.object.isRequired,
+	trueId: PropTypes.bool.isRequired
 }
 export default connect(mapStateToProps)(PollDetail)
